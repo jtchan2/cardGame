28f4b4b35f6af044188b46195423308a2cfcfc72
Justin Chan <61221355+jtchan2@users.noreply.github.com>
2022-07-16 10:50:18 -0700

    Update blackjack.java

--- blackjack.java
+++ blackjack.java
@@ -2,7 +2,8 @@ import java.util.ArrayList;
 
 public class blackjack {
     private deck mainDeck;
-    private final int harddeck=21;
+    private final int BJ=21;
+    private final int houseStand=17;
     public blackjack(){
         mainDeck= new deck();
     }
@@ -15,27 +16,76 @@ public class blackjack {
         ArrayList<card> House= new ArrayList<card>();
         int p1Score=0;
         int houseScore=0;
+        String userInput="";
+        boolean stop=false;
         card holder= mainDeck.giveCard();
         P1.add(holder);
         holder=mainDeck.giveCard();
         House.add(holder);
-        p1Score=checkHandWorth(P1);
-        houseScore=checkHandWorth(House);
-        if(p1Score==21 &&houseScore==21){
-            System.out.println("Draw");
-        }else if(houseScore==21){
-            System.out.println("House Wins");
-        }
-        else if(p1Score==21){
-            System.out.println("Player wins");
-        }
+        p1Score=getHandWorth(P1);
+        houseScore=getHandWorth(House);
+        if(blackJack(houseScore)){
+            if(blackJack(p1Score)){
+                System.out.println("PUSH");
+            }else{
+                System.out.println("House Wins Black Jack");
+            }
+        }else{
+            if(blackJack(p1Score)){
+                System.out.println("Player wins Black Jack");
+                return;
+            }
+            while(stop==false && p1Score<=21){
+                System.out.println("Hit?");
+                if(userInput.toUpperCase()=="Y"){
+                    P1.add(mainDeck.giveCard());
+                    p1Score=getHandWorth(P1);
+                }else{
+                    stop=true;
+                }
+                if(p1Score>21){
+                    System.out.println("User Loses, Broke");
+                }
+            }
 
+        }
     }
-    public int checkHandWorth(ArrayList<card> hand){
+    public boolean overtop(int points){
+        if(points>21){return true;}
+        return false;
+    }
+    public boolean houseMax(int points){
+        if(points>=houseStand){
+            return true;
+        }
+        return false;
+    }
+    public boolean blackJack(int points){
+        if(points==BJ){
+            return true;
+        }
+        return false;
+    }
+    public ArrayList<card> split(ArrayList<card> hand){
+        ArrayList<card> hand2= new ArrayList<card>();
+        hand2.add(hand.remove(1));
+        return hand2;
+    }
+    public boolean isSpiltPoss(ArrayList<card> hand){
+        if(hand.get(0).getFace()==hand.get(1).getFace()){
+            return true;
+        }
+        return false;
+    }
+    public int getHandWorth(ArrayList<card> hand){
         int total=0;
         for(int i=0; i<hand.size(); i++){
             total+=hand.get(i).getPoint();
         }
         return total;
+    }
+    public void hit(deck Deck, ArrayList<card> hand){
+        card drawed= Deck.giveCard();
+        hand.add(drawed);
     }
 }
